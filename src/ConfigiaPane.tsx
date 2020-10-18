e65ff4f7498a41a7a2e40893995d7f102770bb1d
John Hatton <hattonjohn@gmail.com>
2020-10-18 08:28:51 -0600

    typo

--- src/ConfigiaPane.tsx
+++ src/ConfigiaPane.tsx
@@ -48,7 +48,6 @@ export const ConfigiaPane: React.FunctionComponent<IConfigiaPaneProps> = (
         {React.cloneElement(c as React.ReactElement<any>, {
           ...c.props,
         })}
-        ;
       </ConfigiaGroupWrapper>
     );
   });
